ce3fc809c85020654c9ae8a9a68f926f2fd17a0a
Caio Márcio de Souza Santos da Silva <cmsssilva@id.uff.br>
2025-07-06 23:19:33 -0300

    add selenium test cadastro produto

--- src/test/java/net/originmobi/pdv/system/SistemaFuncionalTest.java
+++ src/test/java/net/originmobi/pdv/system/SistemaFuncionalTest.java
@@ -51,4 +51,65 @@ public class SistemaFuncionalTest {
         realizarLogin();
         assertTrue(driver.getPageSource().contains("Usuário: gerente"));
     }
+
+    @Test
+    public void testCadastroProduto() {
+        realizarLogin();
+        driver.get("http://localhost:8080/produto/form");
+        new WebDriverWait(driver, 10)
+            .until(ExpectedConditions.visibilityOfElementLocated(By.name("descricao")));
+        driver.findElement(By.name("descricao")).sendKeys("Produto Selenium");
+        driver.findElement(By.name("valor_custo")).sendKeys("10.00");
+        driver.findElement(By.name("valor_venda")).sendKeys("15.00");
+        driver.findElement(By.name("unidade")).sendKeys("UN");
+        driver.findElement(By.name("ncm")).sendKeys("12345678");
+        driver.findElement(By.name("cest")).sendKeys("1234567");
+        WebElement fornecedor = driver.findElement(By.name("fornecedor"));
+        if (fornecedor.getTagName().equals("select")) {
+            java.util.List<WebElement> opts = fornecedor.findElements(By.tagName("option"));
+            if (opts.size() > 1) opts.get(1).click();
+            else if (opts.size() == 1) opts.get(0).click();
+        }
+        WebElement categoria = driver.findElement(By.name("categoria"));
+        if (categoria.getTagName().equals("select")) {
+            java.util.List<WebElement> opts = categoria.findElements(By.tagName("option"));
+            if (opts.size() > 1) opts.get(1).click();
+            else if (opts.size() == 1) opts.get(0).click();
+        }
+        WebElement grupo = driver.findElement(By.name("grupo"));
+        if (grupo.getTagName().equals("select")) {
+            java.util.List<WebElement> opts = grupo.findElements(By.tagName("option"));
+            if (opts.size() > 1) opts.get(1).click();
+            else if (opts.size() == 1) opts.get(0).click();
+        }
+        driver.findElement(By.name("balanca")).sendKeys("SIM");
+        driver.findElement(By.name("ativo")).sendKeys("ATIVO");
+        driver.findElement(By.name("subtributaria")).sendKeys("SIM");
+        driver.findElement(By.name("vendavel")).sendKeys("SIM");
+        WebElement tributacao = driver.findElement(By.name("tributacao"));
+        if (tributacao.getTagName().equals("select")) {
+            java.util.List<WebElement> opts = tributacao.findElements(By.tagName("option"));
+            if (opts.size() > 1) opts.get(1).click();
+            else if (opts.size() == 1) opts.get(0).click();
+        }
+        WebElement modbc = driver.findElement(By.name("modBcIcms"));
+        if (modbc.getTagName().equals("select")) {
+            java.util.List<WebElement> opts = modbc.findElements(By.tagName("option"));
+            if (opts.size() > 1) opts.get(1).click();
+            else if (opts.size() == 1) opts.get(0).click();
+        }
+        WebElement controlaEstoque = driver.findElement(By.name("controla_estoque"));
+        if (controlaEstoque.getTagName().equals("select")) {
+            java.util.List<WebElement> opts = controlaEstoque.findElements(By.tagName("option"));
+            if (opts.size() > 1) opts.get(1).click();
+            else if (opts.size() == 1) opts.get(0).click();
+        }
+        driver.findElement(By.name("data_validade")).sendKeys("01/01/2030");
+        driver.findElement(By.cssSelector("input[type='submit'][name='enviar']")).click();
+        new WebDriverWait(driver, 10)
+            .until(ExpectedConditions.or(
+                ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "Produto Selenium")
+            ));
+        assertTrue(driver.getPageSource().toLowerCase().contains("sucesso") || driver.getPageSource().toLowerCase().contains("produto"));
+    }
 }
