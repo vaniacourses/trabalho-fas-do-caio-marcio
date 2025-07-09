15b77340b89e60f012802df2595878ff1273933b
KenjiDOdate <kdodate@id.uff.br>
2025-07-09 00:22:50 -0300

    Teste unitários Mockito NotaFicalFinalidade e Titulocontroller

--- src/test/java/net/originmobi/pdv/model/NotaFiscalFinalidadeTest.java
+++ src/test/java/net/originmobi/pdv/model/NotaFiscalFinalidadeTest.java
@@ -27,4 +27,55 @@ class NotaFiscalFinalidadeTest {
         NotaFiscalFinalidade finalidade = new NotaFiscalFinalidade();
         assertNotNull(finalidade);
     }
+   
+    @Test
+    void testObjetosDiferentesNaoSaoIguais() {
+        NotaFiscalFinalidade f1 = new NotaFiscalFinalidade();
+        f1.setCodigo(1L);
+        f1.setTipo(2);
+        f1.setDescricao("Devolução");
+        
+        NotaFiscalFinalidade f2 = new NotaFiscalFinalidade();
+        f2.setCodigo(2L);
+        f2.setTipo(3);
+        f2.setDescricao("Venda");
+        
+        
+        assertNotEquals(f1.getCodigo(), f2.getCodigo());
+        assertNotEquals(f1.getTipo(), f2.getTipo());
+        assertNotEquals(f1.getDescricao(), f2.getDescricao());
+    }
+    @Test
+    void testCamposNulos() {
+
+        NotaFiscalFinalidade f = new NotaFiscalFinalidade();
+        f.setCodigo(null);
+        f.setDescricao(null);
+        
+        assertNull(f.getCodigo());
+        assertNull(f.getDescricao());
+        
+        assertEquals(0, f.getTipo());
+    }
+
+    @Test
+    void testOrdemDeUsoDosSetters() {
+
+        NotaFiscalFinalidade f = new NotaFiscalFinalidade();
+        f.setDescricao("Teste");
+        f.setTipo(1);
+        f.setCodigo(100L);
+        assertAll(
+            () -> assertEquals("Teste", f.getDescricao()),
+            () -> assertEquals(1, f.getTipo())
+            );
+        }
+         @Test
+         void testDescricaoNaoAlteradaInesperadamente() {
+            NotaFiscalFinalidade f = new NotaFiscalFinalidade();
+            f.setDescricao("Teste");
+            
+            assertEquals("Teste", f.getDescricao());
+        }
 }
+
